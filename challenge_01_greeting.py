bacbf2b34647071a8409d8f57547aee2773dfdae
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-03-02 15:36:24 -0800

    feat: solve challenge 01 - personalised greeting

---
+++ challenge_01_greeting.py
@@ -0,0 +1,8 @@
+# Ask the user for their first name
+first_name = input("Enter your first name: ")
+
+# Ask the user for their last name
+last_name = input("Enter your last name: ")
+
+# Print a greeting using an f-string
+print(f"Hello, {first_name} {last_name}! Nice to meet you.")
